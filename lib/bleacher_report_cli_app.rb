6d7feea6f4ed45a04b956f33a29511bfffcf131c
bkrigel <benjamin.krigel@gmail.com>
2018-06-05 16:32:36 -0400

    Fleshed out the code in cli.rb

--- lib/bleacher_report_cli_app.rb
+++ lib/bleacher_report_cli_app.rb
@@ -1,6 +1,8 @@
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
 require_relative "./bleacher_report_cli_app/version"
 require_relative './bleacher_report_cli_app/cli'
 
-module BleacherReportCliApp
 
-end
--- lib/bleacher_report_cli_app/cli.rb
+++ lib/bleacher_report_cli_app/cli.rb
@@ -1,75 +1,139 @@
-class BleacherReportCliApp::CLI
+require 'pry'
 
-  def call
-    puts ""
-    puts "+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+"
-    puts "|------------//  Welcome to Ben's Bleacher Report App  \\\\------------|"
-    puts "+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+"
-    puts ""
-    start
-  end
+module BleacherReportCliApp
+  class CLI
 
-  def start
-    print_leagues
-    puts ""
-    league_input = gets.strip
-    print_divisions(league_input)
-    puts ""
-    division_input = gets.strip
-    if division_input == "back"
-      start
-    else
-      print_teams(division_input)
+    attr_accessor :league_input, :division_input, :team_input, :article_input
+
+    def call
+      puts ''
+      puts '<============================================================================>'
+      puts "  >--------------->  Welcome to Ben's Bleacher Report App  <---------------<"
+      puts '<============================================================================>'
+      puts ''
+      start_league
     end
-    puts ""
-    team_input = gets.strip
-    if team_input == "back"
-      #print_teams(division_input)
-    else
-      print_articles(team_input)
+
+    def start_league
+      print_leagues
+      puts ""
+      @league_input = gets.strip
+      # binding.pry
+      if @league_input.to_s.upcase == 'NBA' || 'NFL' || 'MLB' || 'NHL'
+        start_division
+      elsif @league_input.to_s.downcase == 'end' || 'exit'
+        end_message
+      else
+        puts 'Please enter the name of a league or "end" to exit the program.'
+        @league_input = gets.strip
+      end
     end
-    puts ""
+
+    def start_division
+      print_divisions(@league_input)
+      puts ''
+      @division_input = gets.strip
+      if @division_input.to_s.downcase == divisions.to_s.downcase.any?
+        start_team
+      elsif @division_input.to_s.downcase == 'back'
+        start_league
+      elsif @division_input.to_s.downcase == 'end' || 'exit'
+        end_message
+      else
+        puts 'Please enter the name of a division, "back" to see leagues, or "end" to exit the program.'
+        @division_input = gets.strip
+      end
+    end
+
+    def start_team
+      print_teams(@division_input)
+      puts ''
+      @team_input = gets.strip
+      if @team_input.to_s.downcase == teams.to_s.downcase.any?
+        start_article
+      elsif @team_input.to_s.downcase == 'back'
+        start_division
+      elsif @team_input.to_s.downcase == 'end' || 'exit'
+        end_message
+      else
+        puts 'Please enter the name of a team, "back" to see divisions, or "end" to exit the program.'
+        @team_input = gets.strip
+      end
+    end
+
+    def start_article
+      print_articles(@team_input)
+      puts ''
+      @article_input = gets.strip
+      if @article_input.to_s.downcase == 'back'
+        start_team
+      elsif @article_input.to_s.downcase == 'end' || 'exit'
+        end_message
+      else
+        puts 'Please enter "back" to see teams or "end" to exit the program.'
+        @article_input = gets.strip
+      end
+    end
+
+    def print_leagues
+      puts '   +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+'
+      puts ''
+      puts '        ---> NBA'
+      puts '        ---> NFL'
+      puts '        ---> MLB'
+      puts '        ---> NHL'
+      puts ''
+      puts '   +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+'
+      puts '                    * Please enter the name of a league *'
+    end
+
+    def print_divisions(league)
+      puts '   +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+'
+      puts ''
+      puts '        #{league}:'
+      # for each league, print divisions
+      puts '            ---> #{division}'
+      puts ''
+      puts '   +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+'
+
+      puts '                * Please enter the name of a division *'
+      puts '                    OR enter "back" to see leagues'
+    end
+
+    def print_teams(division)
+      puts '   +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+'
+      puts ''
+      puts '        #{league}:'
+      puts '            #{division} Division:'
+      # for each division, print teams
+      puts '                ---> #{team}'
+      puts ''
+      puts '   +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+'
+
+      puts '                  * Please enter the name of a team *'
+      puts '                    OR enter "back" to see divisions'
+    end
+
+    def print_articles(team)
+      puts '   +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+'
+      puts ''
+      puts '        Top 5 Articles for #{team}:'
+      # for each team, print top 5 headlines
+      puts "            ---> #{headline}"
+      puts ''
+      puts '   +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+'
+
+      puts '                    * Enter "back" to see teams *'
+    end
+
+    def end_message
+      puts ''
+      puts '+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+'
+      puts "       Thank you for using Ben's Bleacher Report App! Come back soon!!        "
+      puts '+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+'
+      puts ''
+    end
 
   end
-
-  def print_leagues
-    puts "      ---> NBA"
-    puts "      ---> NFL"
-    puts "      ---> MLB"
-    puts "      ---> NHL"
-    puts ""
-    puts "                * Please enter the name of a league *"
-    puts "  +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+"
-  end
-
-  def print_divisions(league)
-    puts "    #{league}:"
-    # for each league, print divisions
-    puts "      ---> #{division}"
-    puts ""
-    puts "                * Please enter the name of a division *"
-    puts "                    OR enter \"back\" to see leagues"
-    puts "  +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+"
-  end
-
-  def print_teams(division)
-    puts "    #{division} Division:"
-    # for each division, print teams
-    puts "      ---> #{team}"
-    puts ""
-    puts "                  * Please enter the name of a team *"
-    puts "                     OR enter \"back\" to see leagues"
-    puts "  +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+"
-  end
-
-  def print_articles(team)
-    puts "    Top 5 Articles for #{team}:"
-    # for each team, print top 5 headlines
-    # puts "      ---> #{headline}"
-    puts ""
-    puts "                    * Enter \"back\" to see teams *"
-    puts "  +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+"
-  end
-
 end
 
